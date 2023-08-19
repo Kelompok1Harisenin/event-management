4d19a4592c4c5c3da6a60fe514e1c08f67ee0781
Achmad <achfaisal96@gmail.com>
2023-08-19 11:27:36 +0700

    add packages and organizers models

--- src/models/index.js
+++ src/models/index.js
@@ -3,3 +3,4 @@ module.exports.User = require('./user.model');
 module.exports.Role = require('./role.model');
 module.exports.Ticket = require('./ticket.model');
 module.exports.UserRoles = require('./userRoles.model');
+module.exports.Packages = require('./packages.model');
