8483f0e52e6353dd09c5bb2218e56827688214e3
ILHAM TATAYO LIE <70442926+BOcort@users.noreply.github.com>
2023-08-20 22:12:17 +0900

    Add get Event by Title & Remove Event

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -1,12 +1,14 @@
 const httpStatus = require('http-status');
+const { event } = require('../models');
 // const {} = require('../services/event.service');
 const { catchAsync } = require('../utils');
 
 const getevent = catchAsync(async (req, res) => {
   try {
+    const data = await event.findAll();
     return res.status(httpStatus.OK).send({
-      masssage: 'good',
-      data: 'try',
+      masssage: 'Get Data',
+      data,
     });
   } catch (error) {
     return res.status(httpStatus.BAD_REQUEST).send({
@@ -16,4 +18,40 @@ const getevent = catchAsync(async (req, res) => {
   }
 });
 
-module.exports = { getevent };
+const geteventByTitle = catchAsync(async (req, res) => {
+  try {
+    const Eventtitle = req.params.title;
+    const data = await event.findOne({
+      where: { title: Eventtitle },
+    });
+    return res.status(httpStatus.OK).send({
+      masssage: 'Get Data',
+      data,
+    });
+  } catch (error) {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      masssage: 'Nothing data found',
+      data: 'error',
+    });
+  }
+});
+
+const removeEvent = catchAsync(async (req, res) => {
+  try {
+    const { id, title } = req.body;
+    const data = await event.destroy({
+      where: { id, title },
+    });
+    return res.status(httpStatus.OK).send({
+      masssage: 'Data Has been Deleted',
+      data,
+    });
+  } catch (error) {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: 'was not deleted',
+      data: error,
+    });
+  }
+});
+
+module.exports = { getevent, geteventByTitle, removeEvent };
--- src/routes/v1/event.router.js
+++ src/routes/v1/event.router.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const { getevent } = require('../../controllers/event.controller');
+const { getevent, geteventByTitle, removeEvent } = require('../../controllers/event.controller');
 
 const router = express.Router();
 
+// GET
 router.get('/', getevent);
+router.get('/:title', geteventByTitle);
+
+// DELETE
+router.delete('/remove', removeEvent);
+
+module.exports = router;
