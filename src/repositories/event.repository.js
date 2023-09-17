ccefd9c7d63d30dede6da2e2cefc10729b803ab7
jesslyn-ctrl <jsslnjess08@gmail.com>
2023-09-18 00:11:43 +0700

    Fix get event response by adding organizer data and separate repo

--- src/repositories/event.repository.js
+++ src/repositories/event.repository.js
@@ -1,11 +1,61 @@
-const { Event } = require('../models');
+const { Op } = require('sequelize');
+const { Event, Organizer, User, Package } = require('../models');
 const baseRepository = require('./base.repository');
 
-const findById = (eventId) => {
-  return Event.findByPk(eventId);
+const findAllByFilterWithUserAndPackage = (filter = null) => {
+  const { title } = filter;
+  const queryOptions = {
+    include: [
+      {
+        model: Organizer,
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+          {
+            model: Package,
+            attributes: ['name'],
+          },
+        ],
+      },
+    ],
+  };
+
+  if (title) {
+    queryOptions.where = {
+      title: {
+        [Op.iLike]: `%${title}%`,
+      },
+    };
+  }
+
+  return Event.findAll(queryOptions);
+};
+
+const findByIdWithUserAndPackage = (eventId) => {
+  return Event.findOne({
+    where: { id: eventId },
+    include: [
+      {
+        model: Organizer,
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+          {
+            model: Package,
+            attributes: ['name'],
+          },
+        ],
+      },
+    ],
+  });
 };
 
 module.exports = {
-  findById,
   ...baseRepository,
+  findAllByFilterWithUserAndPackage,
+  findByIdWithUserAndPackage,
 };
