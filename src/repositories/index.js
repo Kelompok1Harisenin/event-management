87b6f78e06182c745399d8a16711c98f548546f9
jesslyn-ctrl <jsslnjess08@gmail.com>
2023-09-23 22:40:51 +0700

    Update login response - show permissions and roles

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,6 +1,7 @@
 module.exports.userRepository = require('./user.repository');
 module.exports.tokenRepository = require('./token.repository');
 module.exports.roleRepository = require('./role.repository');
+module.exports.userRoleRepository = require('./userRole.repository');
 module.exports.packageRepository = require('./package.repository');
 module.exports.organizerRepository = require('./organizer.repository');
 module.exports.eventRepository = require('./event.repository');
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,3 +1,5 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/database');
 const { User } = require('../models');
 const baseRepository = require('./base.repository');
 
@@ -5,7 +7,30 @@ const findByEmail = (email) => {
   return User.findOne({ where: { email } });
 };
 
+const findOneWithRoleAndPermissions = (userId) => {
+  return sequelize.query(
+    `
+    SELECT u.id, u."name", u.email,
+           jsonb_agg(DISTINCT r."name") AS role,
+           jsonb_agg(DISTINCT p."action") AS action,
+           u."createdAt", u."updatedAt"
+    FROM users u
+    JOIN user_roles ur ON u.id = ur."userId"
+    JOIN roles r ON r.id = ur."roleId"
+    JOIN role_permissions rp ON rp."roleId" = ur."roleId"
+    JOIN permissions p ON rp."permissionId" = p.id
+    WHERE u.id = :userId
+    GROUP BY u.id, u."name", u.email, u."createdAt", u."updatedAt"
+    `,
+    {
+      replacements: { userId }, // Parameter binding for :userId
+      type: Sequelize.QueryTypes.SELECT, // Query type for selecting data
+    }
+  );
+};
+
 module.exports = {
   ...baseRepository,
   findByEmail,
+  findOneWithRoleAndPermissions,
 };
