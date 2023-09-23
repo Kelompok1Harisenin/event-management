e89b98025f298157b1b09c9c228d1212cab78438
jesslyn-ctrl <jsslnjess08@gmail.com>
2023-09-23 18:34:07 +0700

    Fix error stack for dev mode

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -7,13 +7,15 @@ const errorConverter = (err, req, res, next) => {
   let error = err;
   if (!(error instanceof ApiError)) {
     const statusCode = error.statusCode ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+    // eslint-disable-next-line security/detect-object-injection
     const message = error.message || httpStatus[statusCode];
     error = new ApiError(statusCode, message, false, err.stack);
   }
   next(error);
 };
 
-const errorHandler = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
   if (config.env === 'production' && !err.isOperational) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
